ea1ecac0d92f1bd4df99b0dc0aedd191f44da39b
Andres Ontiveros <andres.ontiveros@easymetering.com>
2024-02-11 10:58:59 -0500

    [ADD] Frontend project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            {/* <h2 className="title">Río de Decisiones</h2> */}
+            
+        </>
+    );
+};
+
+export default App;
