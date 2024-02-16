8549c2bafd5fb71f31c498a44449e235891432c4
Andres Ontiveros <andres.ontiveros@easymetering.com>
2024-02-16 16:51:56 -0500

    [UPDATE] Styles

--- frontend/src/components/Base.jsx
+++ frontend/src/components/Base.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Avatar } from 'antd';
+import { Layout, Avatar, Dropdown, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
 import UisekLogo from '../images/LOGO-UISEK-web-387x143-1.png';
@@ -8,18 +8,44 @@ const { Header, Content, Footer } = Layout;
 
 
 const Base = ({ children }) => {
+
+    const Logout = () => {
+        localStorage.clear();
+        window.location.reload();
+    };
+
+    const items = [
+        {
+          key: '1',
+          label: (
+            <Button onClick={() => Logout()}>
+              Logout
+            </Button>
+          ),
+        },
+      ];
+
     return (
         <Layout>
             <Header
                 style={{
                 display: 'flex',
                 alignItems: 'center',
+                "justify-content": "space-between"
                 }}
             >
                 <div className="demo-logo" style={{marginRight: '30px', marginTop: '20px', width: '150px'}}>
                     <img src={UisekLogo} alt='logo' style={{width: '100%'}}></img>
                 </div>
-                <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                <Dropdown
+                    menu={{
+                        items,
+                    }}
+                    placement="bottom"
+                    arrow
+                    >
+                    <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                </Dropdown>
                 {/* <Menu
                     theme="dark"
                     mode="horizontal"
